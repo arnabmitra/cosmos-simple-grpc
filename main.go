19ce1f5f7c2d2bd633f155107733b0c5efa144d4
arnabmitra <arnabmitra.mitra@gmail.com>
2023-03-14 11:15:44 -0600

    super simple one file project to run grpc queries against a running cosmos node.

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"google.golang.org/grpc"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+)
+
+func queryState() error {
+	// Create a connection to the gRPC server.
+	grpcConn, err := grpc.Dial(
+		"35.223.43.127:9090", // your gRPC server address.
+		grpc.WithInsecure(),  // The Cosmos SDK doesn't support any transport security mechanism.
+		// This instantiates a general gRPC codec which handles proto bytes. We pass in a nil interface registry
+		// if the request/response types contain interface instead of 'nil' you should pass the application specific codec.
+		grpc.WithDefaultCallOptions(grpc.ForceCodec(codec.NewProtoCodec(nil).GRPCCodec())),
+	)
+	if err != nil {
+		return err
+	}
+	defer grpcConn.Close()
+
+	// This creates a gRPC client to query the x/bank service.
+	bankClient := banktypes.NewQueryClient(grpcConn)
+	bankRes, err := bankClient.Balance(
+		context.Background(),
+		&banktypes.QueryBalanceRequest{Address: "tp1npgzrwjpl09c7fr7p8jt56vyw4jh77k4lpexnv", Denom: "vspn"},
+	)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(bankRes.GetBalance()) // Prints the account balance
+
+	return nil
+}
+
+func main() {
+
+	for i := 0; i < 1000; i++ {
+		fmt.Printf("loop number is %d\n", i)
+		if err := queryState(); err != nil {
+			panic(err)
+		}
+	}
+}
